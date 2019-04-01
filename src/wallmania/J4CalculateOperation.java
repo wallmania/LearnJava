5d1328cc4c67e77e3b97c4b255fc98ad84c514e4
BuildTools <unconfigured@null.spigotmc.org>
2019-04-01 14:53:27 +0900

    Edit

--- src/wallmania/J4CalculateOperation.java
+++ src/wallmania/J4CalculateOperation.java
@@ -1,6 +1,9 @@
 package wallmania;
 
-public class LearnCalculate {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class J4CalculateOperation {
 	public static void main(String[] args) {
 		int x = 10; 
 		int y = 10;
@@ -34,7 +37,16 @@ public class LearnCalculate {
 		System.out.println("x : " + x);
 		x = 10;
 		System.out.println("x-- : " + (x--));
-		System.out.println("x : " + x);		
+		System.out.println("x : " + x);	
+		
+		// -- File 입출력 Code -- System.out.println("Working Directory = " + System.getProperty("user.dir"));
+		FileInputStream fis = null; FileOutputStream fos = null;
+		try {
+			fis = new FileInputStream("/Users/junlee/Java/workspace/LearnJava/src/wallmania/J4CalculateOperation.java");
+			fos = new FileOutputStream("/Users/junlee/Java/workspace/LearnJava/src/wallmania/J4CalculateOperation.txt");
+			int readData = -1; while((readData = fis.read()) != -1) { fos.write(readData); }
+		} catch (Exception e) {  e.printStackTrace();
+		} finally { try { fos.close(); } catch (Exception e) { e.printStackTrace(); } }
 	}
 
 }
